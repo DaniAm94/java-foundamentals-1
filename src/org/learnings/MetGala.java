458c38ea5bef48ee4d90639bf0ee1a04d8037f5c
DaniAm94 <dani.amico94@gmail.com>
2024-05-15 14:59:21 +0200

    completed bonus exercise

---
+++ src/org/learnings/MetGala.java
@@ -0,0 +1,33 @@
+package org.learnings;
+
+import java.util.Scanner;
+
+public class MetGala {
+    public static void main(String[] args) {
+        String[] guests= {
+                "Jennifer Lopez",
+                "Zendaya",
+                "Chris Hemsworth",
+                "Bad Bunny",
+                "Cara Delavigne",
+                "Kendall Jenner",
+                "Uma Thurman",
+                "Damiano David",
+                "Luca Guadagnino"
+        };
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Come ti chiami? ");
+        String name= scanner.nextLine();
+        Boolean isInList= false;
+        for (int i = 0; i < guests.length; i++) {
+            if(name.equals(guests[i]) ){
+                isInList= true;
+            }
+        }
+        if (isInList){
+            System.out.println("Sei nella lista degli invitati. Puoi entrare");
+        }else {
+            System.out.println("Non sei nella lista degli invitati. Non puoi entrare");
+        }
+    }
+}
