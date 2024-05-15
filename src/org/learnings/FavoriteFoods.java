5c8fee188045bab5414dbfcba3b3a3757c7916ed
DaniAm94 <dani.amico94@gmail.com>
2024-05-15 14:35:34 +0200

    completed second exercise

---
+++ src/org/learnings/FavoriteFoods.java
@@ -0,0 +1,24 @@
+package org.learnings;
+
+import java.util.Scanner;
+
+public class FavoriteFoods {
+    public static void main(String[] args) {
+        String[] foods = {
+                "Pizza",
+                "Pasta",
+                "Sushi",
+                "Hamburger",
+                "Tacos",
+                "Gelato",
+                "Pane",
+                "Insalata",
+                "Pollo",
+                "Frutta"
+        };
+        System.out.println("Lunghezza della classifica: " + foods.length);
+        System.out.println("Cibo in cima alla classifica: " + foods[0]);
+        System.out.println("Ultimo cibo della classifica: " + foods[foods.length-1]);
+        System.out.println("Cibo a metà della classifica: " + foods[foods.length/2]);
+    }
+}
