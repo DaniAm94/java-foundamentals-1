82dd2d4cb7c11b23d22b3f84cfacb64c6e45ed83
DaniAm94 <dani.amico94@gmail.com>
2024-05-15 14:20:04 +0200

    completed first exercise

---
+++ src/org/learnings/CalcolaBiglietto.java
@@ -0,0 +1,23 @@
+package org.learnings;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Quanti kilometri devi percorrere? ");
+        int distance= Integer.parseInt(scanner.nextLine());
+        System.out.print("Inserisci la tua età ");
+        int age= Integer.parseInt(scanner.nextLine());
+        double pricePerKm= 0.21;
+        double fullPrice= pricePerKm * distance;
+        double finalPrice = fullPrice;
+        if(age<18){
+            finalPrice = finalPrice * 0.8;
+        }else if(age >65){
+            finalPrice= finalPrice * 0.6;
+        }
+        System.out.println("Il prezzo del tuo biglietto è: " + finalPrice + "€");
+
+    }
+}
